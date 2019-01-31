77b758993ede47e13fdebcb990d9d0714e7370bb
Jacob Straszynski <jacob.straszynski@planet.com>
2019-01-31 12:11:50 -0800

    don't build the image twice
    Just build it once and retag the latest image using the VERSION just
    built. Also fail the script on errors.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 VERSION=$(git rev-parse --short HEAD)
-docker build --tag "planetlabs/draino:latest" .
 docker build --tag "planetlabs/draino:${VERSION}" .
+docker tag "planetlabs/draino:${VERSION}" "planetlabs/draino:latest"
